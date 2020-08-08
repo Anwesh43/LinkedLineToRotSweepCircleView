08a50de4e86aaf46c382e47c480a7bf7c55de636
Anwesh43 <anweshthecool0@gmail.com>
2020-08-08 16:57:36 +0530

    adding LRTSNode class

--- linetorotsweepcircleview/src/main/java/com/anwesh/uiprojects/linetorotsweepcircleview/LineToRotSweepCircleView.kt
+++ linetorotsweepcircleview/src/main/java/com/anwesh/uiprojects/linetorotsweepcircleview/LineToRotSweepCircleView.kt
@@ -119,4 +119,41 @@ class LineToRotSweepCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTRSNode(var i : Int, val state : State = State()) {
+
+        private var next : LTRSNode? = null
+        private var prev : LTRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTRSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTRSNode {
+            var curr : LTRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
