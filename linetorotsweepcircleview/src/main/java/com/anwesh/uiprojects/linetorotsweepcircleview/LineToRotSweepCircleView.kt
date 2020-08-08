a9793f9b62c9adb264073913e4beab56fa24b30e
Anwesh43 <anweshthecool0@gmail.com>
2020-08-08 16:44:45 +0530

    adding State class

--- linetorotsweepcircleview/src/main/java/com/anwesh/uiprojects/linetorotsweepcircleview/LineToRotSweepCircleView.kt
+++ linetorotsweepcircleview/src/main/java/com/anwesh/uiprojects/linetorotsweepcircleview/LineToRotSweepCircleView.kt
@@ -22,6 +22,7 @@ val parts : Int = 4
 val lineDeg : Float = 90f
 val sweepDeg : Float = 360f
 val startDeg : Float = -90f
+val scGap : Float = 0.02f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -69,5 +70,25 @@ class LineToRotSweepCircleView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
