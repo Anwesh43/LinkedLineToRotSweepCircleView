cfd751c6042643134b17c49649b74a9ee02ea4f0
Anwesh43 <anweshthecool0@gmail.com>
2020-08-08 16:26:36 +0530

    adding constants and libraries

--- linetorotsweepcircleview/src/main/java/com/anwesh/uiprojects/linetorotsweepcircleview/LineToRotSweepCircleView.kt
+++ linetorotsweepcircleview/src/main/java/com/anwesh/uiprojects/linetorotsweepcircleview/LineToRotSweepCircleView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.linetorotsweepcircleview
 /**
  * Created by anweshmishra on 08/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf("", "", "", "", "")
+val sizeFactor : Float = 5.8f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 4
